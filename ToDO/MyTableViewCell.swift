4b179563d48a47c3160cc9136f14fae472a36478
Student <student@SETASs-Mac-mini-2.local>
2017-12-19 20:39:32 -0500

    basic structure

---
+++ ToDO/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  ToDO
+//
+//  Created by Student on 2017-12-19.
+//  Copyright © 2017 Centennial. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
